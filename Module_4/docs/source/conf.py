98c62bcf65403226aa9d54b53f3a3ba9739376ea
Darren Agapay <dtagapay@gmail.com>
2025-06-11 17:27:46 -0400

    sphinx initiation

---
+++ Module_4/docs/source/conf.py
@@ -0,0 +1,37 @@
+# Configuration file for the Sphinx documentation builder.
+#
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../src'))
+
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Pizza Ordering System'
+copyright = '2025, Darren Agapay'
+author = 'Darren Agapay'
+release = '1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.napoleon']
+              
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+
+
---
+++ Module_4/docs/source/index.rst
@@ -0,0 +1,17 @@
+.. Pizza Ordering System documentation master file, created by
+   sphinx-quickstart on Wed Jun 11 17:13:47 2025.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Pizza Ordering System
+=====================
+
+.. automodule:: pizza
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+.. automodule:: order
+   :members:
+   :undoc-members:
+   :show-inheritance:
